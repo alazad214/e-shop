5e0438e765a58fe7f1a712b466d1331a57474f46
al azad <alazad214@gmail.com>
2024-07-15 09:23:00 +0600

    firebase addign

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:e_shop/auth/login.dart';
 import 'package:e_shop/pages/bottomNav.dart';
 import 'package:e_shop/pages/product_details.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -16,9 +23,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Login()
-    );
+    return const GetMaterialApp(
+        debugShowCheckedModeBanner: false, home: Login());
   }
 }
