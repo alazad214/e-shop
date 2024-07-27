f2e645210030ff9b8a19c37ed5cbe79bd69fe052
al azad <alazad214@gmail.com>
2024-07-27 19:44:48 +0600

    code modify

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:e_shop/auth/login.dart';
+import 'package:e_shop/pages/bottomNav.dart';
 import 'package:e_shop/pages/onboading.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -14,15 +19,20 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-        debugShowCheckedModeBanner: false, home: Onboading());
+    final user = FirebaseAuth.instance.currentUser;
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: user == null ? Onboading() : Bottomnav(),
+    );
   }
 }
